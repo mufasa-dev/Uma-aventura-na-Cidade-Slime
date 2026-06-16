cbfd1b4ed956e475e0fae63d83d530c29a9724f0
Bruno <bruno.ribeiro96@hotmail.com.br>
2026-06-16 12:31:17 -0300

    assets: ajust jose slime

--- novo-projeto-de-jogo/scripts/player.gd
+++ novo-projeto-de-jogo/scripts/player.gd
@@ -1,28 +1,65 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 200
+const JUMP_VELOCITY = 10
+@onready var animator = get_node("jose/AnimationPlayer") as AnimationPlayer
 
+@export var view : Node3D
+
+var gravity = 0
+var movement_velocity : Vector3
+var rotation_direction : float
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	handle_input(delta)
+	apply_gravity(delta)
+	jump(delta)
+	handle_animations()
+	
+	var applied_velocity : Vector3
+	applied_velocity = velocity.lerp(movement_velocity, delta * 10)
+	applied_velocity.y = -gravity
+	
+	velocity = applied_velocity
+	
 	move_and_slide()
+	
+	if Vector2(velocity.z, velocity.x).length() > 0:
+		rotation_direction = Vector2(velocity.z, velocity.x).angle()
+		
+	rotation.y = lerp_angle(rotation.y, rotation_direction, delta * 10)
+
+func handle_input(delta):
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_backward")
+	
+	input = input.rotated(Vector3.UP, view.rotation.y).normalized()
+	
+	velocity = input * SPEED * delta
+	
+func handle_animations():
+	if is_on_floor():
+		if abs(velocity.x) > 1 or abs(velocity.z) > 1:
+			animator.play("Walk", 0.3)
+		else:
+			animator.play("Idle", 0.3)
+	else:
+		animator.play("Jump", 2)
+		
+	if !is_on_floor() and gravity > 2:
+		animator.play("Idle", 0.3) ## trocar para fall
+	
+func apply_gravity(delta):
+	if not is_on_floor():
+		gravity += 25 * delta
+		
+	if gravity > 0 and is_on_floor():
+		gravity = 0
+	
+func jump(delta):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		gravity = -JUMP_VELOCITY
+	
+	
